e1e1fff53884a53a3ad2759c04522342f97d536d
abdelhamid.belgacem <abdelhamid.belgacem@ifsalpha.com>
2020-03-31 17:14:56 +0200

    Currency-exchange-Service Test Project 31-03-2020-INVENTIV.

---
+++ src/main/java/com/peterith/currencymicroservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -0,0 +1,32 @@
+package com.peterith.currencymicroservices.currencyexchangeservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CurrencyExchangeController {
+
+    private final CurrencyExchangeService currencyExchangeService;
+    private final Environment environment;
+
+    @Autowired
+    public CurrencyExchangeController(CurrencyExchangeService currencyExchangeService, Environment environment) {
+        this.currencyExchangeService = currencyExchangeService;
+        this.environment = environment;
+    }
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public ResponseEntity getExchangeValue(@PathVariable String from, @PathVariable String to) {
+
+        return currencyExchangeService.findByFromAndTo(from, to)
+                .map(exchangeValue -> {
+                    exchangeValue.setPort(Integer.parseInt(environment.getProperty("server.port")));
+                    return ResponseEntity.ok(exchangeValue);
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+}
---
+++ src/main/java/com/peterith/currencymicroservices/currencyexchangeservice/ExchangeValue.java
@@ -0,0 +1,31 @@
+package com.peterith.currencymicroservices.currencyexchangeservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ExchangeValue {
+
+    @Id
+    private Long id;
+
+    @Column(name="currency_from")
+    private String from;
+
+    @Column(name="currency_to")
+    private String to;
+
+    private BigDecimal conversionMultiple;
+    private int port;
+}
